439bfda3b0b8a8c719a01d5d731a40e263a0cda8
Ravid Cohen <ravidd9@gmail.com>
2019-05-18 14:45:19 +0300

    paging end fixed 2

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -10,6 +10,7 @@ class Clients extends Component {
         super()
         this.state = {
             clients: [],
+            filteredClients: [],
             input: "",
             select: "",
             viewCount: 1
@@ -17,20 +18,25 @@ class Clients extends Component {
     }
 
     handleViewCount = (increase) => {
-        if (increase && this.state.viewCount < this.state.clients.length) {
-            this.setState({ viewCount: this.state.viewCount + 20 })
+        let filteredClients = this.state.filteredClients
+        if (increase && this.state.viewCount + 20 <= this.state.clients.length) {
+            if (!this.state.input || (this.state.input && this.state.viewCount + 20 <= filteredClients.length)) {
+                this.setState({ viewCount: this.state.viewCount + 20 })
+            }
         } else if (!increase && this.state.viewCount >= 21) {
             this.setState({ viewCount: this.state.viewCount - 20 })
         }
     }
 
-    saveValues = (input, select) => this.setState({ input, select , viewCount: 1})
+    saveValues = (input, select) => this.setState({ input, select, viewCount: 1, filteredClients: this.getFilteredClients(input, select) })
+
+    getFilteredClients = (input, select) => this.state.clients.filter(c => c[select].toLowerCase().includes(input))
 
     getClients = async () => {
         let clients = await axios.get('http://localhost:8000/clients')
         return clients.data
     }
- 
+
     componentDidMount = async () => {
         let clients = await this.getClients()
         this.setState({ clients })
@@ -46,16 +52,22 @@ class Clients extends Component {
 
     render() {
         let viewCount = this.state.viewCount
+        let clients = this.state.clients
+        let filteredClients = this.state.filteredClients
         return (
             <div>
                 <SearchClients saveValues={this.saveValues} />
-                <MovePages viewCount={viewCount} handleViewCount={this.handleViewCount} />
+                <MovePages
+                    viewCount={viewCount}
+                    handleViewCount={this.handleViewCount}
+                    length={filteredClients.length ? filteredClients.length : clients.length}
+                />
                 <ClientsFields />
                 {this.state.input ?
-                    this.state.clients.filter(c => c[this.state.select].toLowerCase().includes(this.state.input))
+                    clients.filter(c => c[this.state.select].toLowerCase().includes(this.state.input))
                         .slice(viewCount - 1, viewCount + 19)
                         .map((client, i) => <ClientRow key={i} client={client} />) :
-                    this.state.clients.slice(viewCount - 1, viewCount + 19).map((client, i) => <ClientRow key={i} client={client} />)}
+                    clients.slice(viewCount - 1, viewCount + 19).map((client, i) => <ClientRow key={i} client={client} />)}
             </div>
         );
     }
--- src/components/Clients/MovePages.js
+++ src/components/Clients/MovePages.js
@@ -5,14 +5,17 @@ import '../../style/MovePages.css';
 class MovePages extends Component {
 
     increase = () => this.props.handleViewCount(true)
-    
+
     decrease = () => this.props.handleViewCount(false)
 
     render() {
         return (
             <div id="movePages">
                 <i className="fas fa-arrow-left" onClick={this.decrease}></i>
-                {this.props.viewCount}  -  {this.props.viewCount + 19}
+                {this.props.viewCount}  -
+                {this.props.length >= this.props.viewCount + 19 ?
+                    this.props.viewCount + 19 :
+                    "END"}
                 <i className="fas fa-arrow-right" onClick={this.increase}></i>
             </div>
         );
