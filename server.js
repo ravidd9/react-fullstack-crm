6f08c6320448c465fb1715e4821b31411ecd5917
Ravid Cohen <ravidd9@gmail.com>
2019-05-19 12:02:21 +0300

    db?

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmDB", { useNe
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-    next()
-})
+//     next()
+// })
 app.use('/', api)
 
 
